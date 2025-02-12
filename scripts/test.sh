64795830a56f348b269aee3b92920d0183cdcbdb
Tom Augspurger <toaugspurger@nvidia.com>
2025-02-12 10:40:19 -0800

    Updated pre-commit hook

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,23 +2,23 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES.
 
 echo "[testing dask]"
-pushd dask
+pushd dask || exit 1
 pytest dask -v -m gpu
 dask_status=$?
-popd
+popd || exit 1
 
 echo "[testing distributed]"
-pushd distributed
+pushd distributed || exit 1
 pytest distributed -v -m gpu --runslow
 distributed_status=$?
-popd
+popd || exit 1
 
 echo "[testing downstream]"
 
-pushd downstream
+pushd downstream || exit 1
 pytest -v .
 downstream_status=$?
-popd
+popd || exit 1
 
 if [ $dask_status -ne 0 ] || [ $distributed_status -ne 0 ] || [ $downstream_status -ne 0 ] ; then
     echo "Tests faild"
