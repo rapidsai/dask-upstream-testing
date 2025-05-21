d523891e0fbe6656732fc37afaa894a650c141de
Tom Augspurger <toaugspurger@nvidia.com>
2025-05-21 14:56:38 -0700

    Pin to python from uv 7.5

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,7 +9,12 @@ if ! command -v uv > /dev/null; then
     source $HOME/.local/bin/env
 fi
 
-uv venv --allow-existing --python 3.12 --managed-python
+# Temporarily pin to uv 7.5's python-build-standalone.
+# Follow https://github.com/astral-sh/python-build-standalone/issues/619
+# and https://github.com/rapidsai/dask-upstream-testing/issues/56 for details.
+uvx uv@0.7.5 python install --reinstall 3.12
+uv venv --allow-existing --python 3.12 --managed-python --no-cache
+
 source .venv/bin/activate
 
 ./scripts/install.sh
