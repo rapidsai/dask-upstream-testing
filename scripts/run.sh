b5a93439cb379aac018c3b009f346ed9a09730a0
Tom Augspurger <toaugspurger@nvidia.com>
2025-06-03 12:22:06 -0700

    Revert uv pin

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,11 +9,7 @@ if ! command -v uv > /dev/null; then
     source $HOME/.local/bin/env
 fi
 
-# Temporarily pin to uv 7.5's python-build-standalone.
-# Follow https://github.com/astral-sh/python-build-standalone/issues/619
-# and https://github.com/rapidsai/dask-upstream-testing/issues/56 for details.
-uvx uv@0.7.5 python install --reinstall 3.12
-uv venv --allow-existing --python 3.12 --managed-python --no-cache
+uv venv --allow-existing --python 3.12 --managed-python
 
 source .venv/bin/activate
 
